8736308245e448f5b3885b768ecff1b6a028fbaf
jaiwanth123 <114085858+jaiwanth123@users.noreply.github.com>
2024-06-08 07:45:34 +0530

    Delete core -ether -challenges directory

--- core -ether -challenges/get_blockNumber.js
+++
@@ -1,9 +0,0 @@
-const { ethers } = require('ethers');
-const provider = new ethers.providers.JsonRpcProvider("https://rpc.coredao.org");
-
-const get_blocknumber =async()=>{
-
-    const tx=await provider.getBlockNumber();
-    console.log(tx);
-}
-get_blocknumber();
--- core -ether -challenges/get_transactioncount.js
+++
@@ -1,9 +0,0 @@
-const { ethers } = require('ethers');
-const provider = new ethers.providers.JsonRpcProvider("https://rpc.coredao.org");
-
-const get_transactioncount =async()=>{
-
-    const tx=await provider.getTransactionCount("0x1FB9Becc1356FCC6E70792B755e5c76e3Cb26623");
-    console.log(tx);
-}
-get_transactioncount();
